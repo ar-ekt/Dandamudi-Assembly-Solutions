53840376f97145992d208b729b312d28f045e87f
Amir <amirgh1380@gmail.com>
2021-02-10 13:41:31 +0330

    updated P14-01.asm

--- Codes/Chapter14/P01/P14-01.asm
+++ Codes/Chapter14/P01/P14-01.asm
@@ -1,27 +1,27 @@
+; Should run in linux, add ExitProcess in windows-os
 %INCLUDE "lib.h" ; no usage
 global _start
 
-%macro strlen 1
-    push    ebx
+%macro PutStr 1
+    pushad
+    xor ecx, ecx
+    mov esi, %1
+    %%_strlen:
+        inc ecx
+        lodsb
+        cmp al, 0
+        jne %%_strlen
+    dec ecx
 
-    mov     ebx, %1         ; move the address of our message string into EBX
-    mov     eax, ebx        ; move the address in EBX into EAX as well (Both now point to the same segment in memory)
+    ; sys_write
+    mov	edx, ecx    ; message length
+    mov	ecx, %1 	; message to write
+    mov	ebx, 1		; file descriptor (stdout)
+    mov	eax, 4		; system call number (sys_write)
+    int	0x80		; call kernel  
 
-nextchar:
-    cmp     byte [eax], 0   ; compare the byte pointed to by EAX at this address against zero (Zero is an end of string delimiter)
-    jz      finished        ; jump (if the zero flagged has been set) to the point in the code labeled 'finished'
-    inc     eax             ; increment the address in EAX by one byte (if the zero flagged has NOT been set)
-    jmp     nextchar        ; jump to the point in the code labeled 'nextchar'
-
-finished:
-    sub     eax, ebx        ; subtract the address in EBX from the address in EAX
-                            ; remember both registers started pointing to the same address (see line 15)
-                            ; but EAX has been incremented one byte for each character in the message string
-                            ; when you subtract one memory address from another of the same type
-                            ; the result is number of segments between them - in this case the number of bytes
-    pop     ebx
+    popad
 %endmacro
-
 
 SIZE equ 30 ; Maximum string size
 
@@ -42,7 +42,6 @@ _start:
     mov eax, 4
     int 0x80
 
-
     ; sys_read      ; fgets string, SIZE
     mov ebx, 2
     mov ecx, string  
@@ -50,14 +49,7 @@ _start:
     mov eax, 3
     int 80h
 
-    strlen string   ; result stores in eax register
-
-    ; sys_write     ; puts string
-    mov	edx,eax		; message length
-    mov	ecx,string	; message to write
-    mov	ebx,1		; file descriptor (stdout)
-    mov	eax,4		; system call number (sys_write)
-    int	0x80		; call kernel        
+    PutStr string  
 
 exit:
     mov eax, 1
