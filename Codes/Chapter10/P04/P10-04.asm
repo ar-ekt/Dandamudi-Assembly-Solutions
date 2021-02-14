42553c4327681450fa8703114908f020d4da20b2
Amirreza Ektesabi <57006850+ar-ekt@users.noreply.github.com>
2021-02-14 17:05:03 +0330

    remove buffer in P10-04

--- Codes/Chapter10/P04/P10-04.asm
+++ Codes/Chapter10/P04/P10-04.asm
@@ -7,11 +7,9 @@ section .data
     NEWLINE db 10, 0
     
     MSG_STRING_INPUT db "Enter string: ", 0
-    
     MSG_OUTPUT db "Result: ", 0
     
 section .bss
-    buffer resb MAX_STRING_SIZE
     string resb MAX_STRING_SIZE
 
 section .code
@@ -20,7 +18,7 @@ _start:
     fgets string, MAX_STRING_SIZE
     
     push string
-    call str_cln_leading_blnks
+    call remove_leading_blanks
     
     puts MSG_OUTPUT
     puts string
@@ -30,7 +28,7 @@ _end:
     push DWORD 0
     call ExitProcess
 
-str_cln_leading_blnks:
+remove_leading_blanks:
     %define string DWORD [EBP+8]
     enter 0,0
     push ECX
@@ -41,33 +39,25 @@ str_cln_leading_blnks:
     
     mov ESI, string
     push ESI
-    call str_frst_non_blank
-    jc str_cln_leading_blnks_no_string
+    call first_non_blank
+    jc remove_leading_blanks_no_string
     add ESI, EAX
     
     push ESI
     call str_len
-    jc str_cln_leading_blnks_no_string
-    mov ECX, EAX
-    mov EDI, buffer
-    cld
-    rep movsb
-    
-copy:
-    mov ESI, buffer
-    push ESI
-    call str_len
+    jc remove_leading_blanks_no_string
     mov ECX, EAX
     mov EDI, string
     cld
     rep movsb
+    
     mov [EDI], BYTE 0
-    mov EAX, buffer
+    mov EAX, string
     clc
-    jmp SHORT str_cln_leading_blnks_done
-str_cln_leading_blnks_no_string:
+    jmp SHORT remove_leading_blanks_done
+remove_leading_blanks_no_string:
     stc
-str_cln_leading_blnks_done:
+remove_leading_blanks_done:
     pop ES
     pop DS
     pop ESI
@@ -76,7 +66,7 @@ str_cln_leading_blnks_done:
     leave
     ret 12
 
-str_frst_non_blank:
+first_non_blank:
     %define string DWORD [EBP+8]
     enter 0, 0
     push ECX
@@ -87,15 +77,15 @@ str_frst_non_blank:
     cld
     mov AL, 32
     repe scasb
-    jcxz str_frst_non_blank_no_string
+    jcxz first_non_blank_no_string
     dec EDI
     mov EAX, EDI
     sub EAX, string
     clc
-    jmp SHORT str_frst_non_blank_done
-str_frst_non_blank_no_string:
+    jmp SHORT first_non_blank_done
+first_non_blank_no_string:
     stc
-str_frst_non_blank_done:
+first_non_blank_done:
     pop ES
     pop EDI
     pop ECX
