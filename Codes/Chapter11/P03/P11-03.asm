8d218250e34f1e311d40bf7a35d6c96295828709
Reza Farjam <67030594+reza00farjam@users.noreply.github.com>
2021-03-02 18:28:04 +0330

    edited P11-03

--- Codes/Chapter11/P03/P11-03.asm
+++ Codes/Chapter11/P03/P11-03.asm
@@ -1,3 +1,3 @@
 and al, 0Fh ; mask off the upper four bits to get the unpacked BCD representation of least significant digit
 shl ah, 4   ; shift most significant digit to left by 4 bits to have its unpacked BCD representation in four upper bits
-and al, ah  ; now combine unpacked BCD representations to get packed BCD representation and store it in AL
+or al, ah   ; now combine unpacked BCD representations to get packed BCD representation and store it in AL
